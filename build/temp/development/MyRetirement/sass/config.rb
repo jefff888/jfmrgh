6cebd74274e7391b1ffe63009daa6f58eef63043
Jeff Furgal <Jeff.Furgal@sungard.com>
2014-10-10 11:59:22 -0500

    Try cleanup again.

--- build/temp/development/MyRetirement/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '..\..\..\..\..\ext\packages\ext-theme-base\sass\utils.rb'
-require '..\..\..\..\..\sass\config.rb'
-cache_path = 'c:\Users\Jeff.Furgal\src\mret42/build/.sass-cache'
--- build/temp/development/MyRetirement/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require c:\Users\Jeff.Furgal\src\mret42\app.js
